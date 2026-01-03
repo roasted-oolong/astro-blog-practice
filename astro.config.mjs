acb94020599a032a9f80201745a3a0c7c7052e77
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2026-01-03 02:12:13 -0600

    Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dainty-entremet-014c1e.netlify.app/"
+  site: "https://dainty-entremet-014c1e.netlify.app/",
+  integrations: [preact()]
 });

7750c0a5e456bd5c68345b0a0ae57256ff2fc4a4
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2026-01-02 21:55:01 -0600

    Create RSS feed for my website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://dainty-entremet-014c1e.netlify.app/"
+});
